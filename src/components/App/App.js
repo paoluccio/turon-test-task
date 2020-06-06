6fcc550d5266bc9818ecd37a0562b36d1c74d06d
PavelP <petropavlovskiy@yahoo.com>
2020-06-06 14:02:40 +0500

    reduced boilerplate and installed necessary libraries

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './App.module.css';
+
+const App = () => {
+  return (
+    <div className={styles.container}>
+      Work in progress!
+    </div>
+  );
+};
+
+export default App;
